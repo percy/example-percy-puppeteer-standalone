ccd571bf6beb26a208512be385385569a5529500
Ana Ulin <ana@ulin.org>
2018-11-06 11:58:14 -0800

    Integrate snapshotting with Percy.

--- puppeteer_snapshots.js
+++ puppeteer_snapshots.js
@@ -1,8 +1,9 @@
-// A script to navigate our TodoMVC app using Puppeteer.
+// A script to navigate our app using Puppeteer and take snapshots with Percy.
 
 (async () => {
   const puppeteer = require('puppeteer')
   const httpServer = require('http-server')
+  const { percySnapshot } = require('@percy/puppeteer')
 
   // Start a local HTTP Server to host our TodoMVC app.
   const server = httpServer.createServer()
@@ -16,10 +17,12 @@
 
   // Load app.
   await page.goto(URL);
+  await percySnapshot(page, 'TodoMVC home page')
 
   // Enter a new to-do.
   await page.type('.new-todo', 'Fancy todo from Puppeteer')
   await page.keyboard.press('Enter')
+  await percySnapshot(page, 'TodoMVC with a new todo', { widths: [768, 992, 1200] })
 
   // Close up our Puppeteer browser.
   browser.close()
